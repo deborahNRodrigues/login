a3c87aa49405bca9a373cef75f429b2476ecf50b
Debora Rodrigues <debora.rodrigues@vat.local>
2026-01-05 17:28:01 -0400

    Atualização dos campos de e-mail e senha e inserção do botão de enviar, e estilização de alguns campos já usando o framework materail ui

--- src/login.tsx
+++ src/login.tsx
@@ -3,15 +3,15 @@ import { Input, TextField, Typography } from "@mui/material";
 
 const Login = () => {
     return (
-        <>
-
+      
+             <>
             <Typography variant="h2" align="center">Login</Typography>
 
             <TextField id="outlined-basic" label="E-mail" required variant="outlined" />
 
             <TextField type="password" id="outlined-basic" label="******" variant="outlined" />
 
-        </>
+      </>  
 
     )
 
